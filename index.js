3347d86ca912a585054187482e0b3644ced87557
NicolaiLoeffler <nicolai.loeffler92@gmail.com>
2015-10-09 13:40:14 +0200

    added reading and deleting of configurations

--- index.js
+++ index.js
@@ -1,95 +1,107 @@
 var Hapi = require('hapi');
+var _ = require('lodash');
 var MongoClient = require('mongodb').MongoClient;
 
 var server = new Hapi.Server();
-var collection;
+var confCollection;
 
 
 // Connect to the db
-MongoClient.connect("mongodb://localhost:27017/exampleDb", function(err, db) {
-    if(!err) {
-        console.log("Connected to MondoDB");
-        db.dropDatabase();
-        collection = db.collection('test', function(err, collection) {});
-    }
+MongoClient.connect("mongodb://127.0.0.1:27017/soga", function(err, db) {
+  if(!err) {
+    console.log("Connected to MongoDB");
+    db.dropDatabase();
+    confCollection = db.collection('configurations', function(err, collection) {
+      if(err) {
+        console.error('couldnt get collection');
+      } else {
+        console.log('got collection configuration');
+      }
+    });
+  } else {
+    console.log('failed connection to MongoDB');
+    console.log(err);
+  }
 });
 
 server.connection({
-    port: 3000,
-    routes: {
-        cors : {
-            origin: ['http://localhost:8100']
-        }
+  port: 3000,
+  routes: {
+    cors : {
+      origin: ['http://localhost:8100']
     }
+  }
 });
 
 server.route({
-    method: 'GET',
-    path: '/configs',
-    handler: function (request, reply) {
-        collection.find().toArray(function(err, items) {
-            if(err){
-                console.log('bad stuff happend');
-            } else {
-                reply(items);
-            }
-        });
-    }
+  method: 'GET',
+  path: '/configs',
+  handler: function (request, reply) {
+    confCollection.find().toArray(function(err, items) {
+      if(err){
+        console.log('bad stuff happend');
+        return;
+      } else {
+        reply(_.map(items, 'name'));
+      }
+    });
+  }
 });
 
 server.route({
-    method: 'GET',
-    path: '/configs/{name}',
-    handler: function (request, reply) {
-        var config = {
-            "id" : "1234",
-            "moisture": "30",
-        }
-        reply(config);
-    }
-});
-
-server.route({
-    method: 'DELETE',
-    path: '/configs/{name}',
-    handler: function (request, reply) {
-      console.info(request.params.name);
-      collection.remove({'name':request.params.name});
-      reply();
-    }
-});
-
-server.route({
-    method: 'GET',
-    path: '/devices',
-    handler: function (request, reply) {
-        var devices = [
-            {
-                "name": "Arduino_1",
-                "config": "Kaktus",
-                "status": "not connected"
-            },
-            {
-                "name": "Arduino_2",
-                "config": "Tomate",
-                "status": "connected"
-            }
-        ];
-        reply(JSON.stringify(devices));
-    }
-});
-
-server.route({
-    method: 'POST',
-    path: '/newConfig',
-    handler: function  (request, reply) {
-        var configName = Object.keys(request.payload)[0];
-        collection.insert(request.payload);
+  method: 'GET',
+  path: '/configs/{name}',
+  handler: function (request, reply) {
+    confCollection.findOne({'name':request.params.name}, function(err, item) {
+      if(!err) {
+        reply(item);
+      } else {
         reply();
-    }
+      }
+    });
+  }
+});
+
+server.route({
+  method: 'DELETE',
+  path: '/configs/{name}',
+  handler: function (request, reply) {
+    confCollection.remove({'name':request.params.name});
+    reply();
+  }
+});
+
+server.route({
+  method: 'GET',
+  path: '/devices',
+  handler: function (request, reply) {
+    var devices = [
+      {
+        "name": "Arduino_1",
+        "config": "Kaktus",
+        "status": "not connected"
+      },
+      {
+        "name": "Arduino_2",
+        "config": "Tomate",
+        "status": "connected"
+      }
+    ];
+    reply(JSON.stringify(devices));
+  }
+});
+
+server.route({
+  method: 'POST',
+  path: '/newConfig',
+  handler: function  (request, reply) {
+    var configName = Object.keys(request.payload)[0];
+    confCollection.insert(request.payload);
+    reply();
+  }
 });
 
 
 server.start(function () {
-    console.log('Server running at:', server.info.uri);
+  console.log('Server running at:', server.info.uri);
 });
